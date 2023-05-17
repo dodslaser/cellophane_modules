d716cae9e196476e6f475f879c9d66b04c2f288c
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-05-17 14:21:46 +0200

    feat: Add start/end mail module

---
+++ modules/mail/README.md
@@ -0,0 +1,56 @@
+# Start/end mail module for Cellophane
+
+Module for sending start/end mail notifications.
+
+## Configuration
+
+Option                 | Type | Required | Default             | Description
+-----------------------|------|----------|---------------------|-------------
+`mail.skip`            | bool |          | false               | Don't send mail
+`mail.start.subject`   | str  |          | See [Start](#Start) | Subject of the mail (jinja2 template)
+`mail.start.body`      | str  |          | See [Start](#Start) | Body of the mail (jinja2 template)
+`mail.start.from_addr` | str  | -        |                     | From address to use
+`mail.start.to_addr`   | list | -        |                     | List of recipients
+`mail.start.cc_addr`   | list | -        |                     | List of CC recipients
+`mail.end.subject`     | str  | -        | See [End](#End)     | Subject of the mail (jinja2 template)
+`mail.end.body`        | str  | -        | See [End](#End)     | Body of the mail (jinja2 template)
+`mail.smtp.host`       | str  | x        |                     | SMTP host
+`mail.smtp.port`       | int  |          | 25                  | SMTP port
+`mail.smtp.tls`        | bool |          | false               | Use TLS
+`mail.smtp.user`       | str  |          |                     | SMTP username
+`mail.smtp.password`   | str  |          |                     | SMTP password
+
+## Hooks
+
+Name                   | When | Condition | Description
+-----------------------|------|-----------|-------------
+`start_mail`           | Pre  |           | Send email when pipeline starts
+`end_mail`             | Post | Always    | Send email when pipeline ends
+
+## Start
+
+**Subject:** "{{ analysis }} started"
+**Body:**
+{{ analysis }} has started for {{ samples.unique_ids|length }} sample(s).
+The following samples are being analyzed:
+{% for id in samples.unique_ids %}
+{{ id }}
+{%- endfor %}
+
+## End
+
+**Subject:** "{{ analysis }} finished"
+**Body:**
+{{ analysis }} has finished processing {{ samples.unique_ids|length }} sample(s).
+{% if samples.failed|length > 0 %}
+❗️ Analysis failed for the following samples:
+{% for id in samples.failed.unique_ids %}
+{{ id }}
+{%- endfor %}
+{% endif %}
+{%- if samples.complete|length > 0 %}
+✅ Analysis completed successfully for the following samples:
+{% for id in samples.complete.unique_ids %}
+{{ id }}
+{%- endfor %}
+{% endif %}
---
+++ modules/mail/__init__.py
@@ -0,0 +1,118 @@
+from logging import LoggerAdapter
+from typing import Optional
+from cellophane import modules, data, cfg
+from smtplib import SMTP
+from email.message import EmailMessage
+from mimetypes import guess_type
+from pathlib import Path
+from jinja2 import Environment
+
+
+def _send_mail(
+    *,
+    from_addr: str,
+    to_addr: list[str] | str,
+    subject: str,
+    body: str,
+    host: str,
+    port: int,
+    tls: bool = False,
+    cc_addr: Optional[list[str] | str] = None,
+    user: Optional[str] = None,
+    password: Optional[str] = None,
+    attachments: Optional[list[Path]] = None,
+    **_,
+) -> None:
+    conn = SMTP(host, port)
+    if tls:
+        conn.starttls()
+    if user and password:
+        conn.login(user, password)
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg["Subject"] = subject
+    msg["From"] = from_addr
+    msg["To"] = ", ".join(to_addr) if isinstance(to_addr, list) else to_addr
+    if cc_addr is not None:
+        msg["Cc"] = ", ".join(cc_addr) if isinstance(cc_addr, list) else cc_addr
+
+    for attachment in attachments or []:
+        ctype, encoding = guess_type(attachment)
+        if ctype is None or encoding is not None:
+            ctype = "application/octet-stream"
+        maintype, subtype = ctype.split("/", 1)
+        with open(attachment, "rb") as fp:
+            msg.add_attachment(
+                fp.read(),
+                maintype=maintype,
+                subtype=subtype,
+                filename=Path(attachment).name,
+            )
+
+    conn.send_message(msg)
+    conn.quit()
+
+
+def _render_mail(subject, body, **kwargs):
+    subject = Environment().from_string(subject).render(**kwargs)
+    body = Environment().from_string(body).render(**kwargs)
+
+    body_template = Environment().from_string(body)
+    subject_template = Environment().from_string(subject)
+
+    subject = subject_template.render(**kwargs)
+    body = body_template.render(**kwargs)
+    return subject, body
+
+
+@modules.pre_hook(label="Send start mail", after="all")
+def start_mail(
+    samples: data.Samples[data.Sample],
+    logger: LoggerAdapter,
+    config: cfg.Config,
+    **_,
+):
+    if not config.mail.skip:
+        logger.debug(f"Sending start mail to {config.mail.start.to_addr}")
+        subject, body = _render_mail(
+            **config.mail,
+            **config.mail.start,
+            analysis=config.analysis,
+            samples=samples,
+        )
+
+        cc_addr = config.mail.start.cc_addr if "cc_addr" in config.mail.start else None
+        _send_mail(
+            **config.mail.smtp,
+            body=body,
+            subject=subject,
+            to_addr=config.mail.start.to_addr,
+            from_addr=config.mail.start.from_addr,
+            cc_addr=cc_addr,
+        )
+
+
+@modules.post_hook(label="Send end mail", condition="always", after="all")
+def end_mail(
+    samples: data.Samples[data.Sample],
+    logger: LoggerAdapter,
+    config: cfg.Config,
+    **_,
+):
+    if not config.mail.skip:
+        logger.debug(f"Sending end mail to {config.mail.end.to_addr}")
+        subject, body = _render_mail(
+            **config.mail,
+            **config.mail.end,
+            analysis=config.analysis,
+            samples=samples,
+        )
+        cc_addr = config.mail.end.cc_addr if "cc_addr" in config.mail.end else None
+        _send_mail(
+            **config.mail.smtp,
+            body=body,
+            subject=subject,
+            to_addr=config.mail.end.to_addr,
+            from_addr=config.mail.end.from_addr,
+            cc_addr=cc_addr,
+        )
