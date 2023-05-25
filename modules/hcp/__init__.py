1018b2d21ededb5f7fd3c35996824f040b62fc05
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-05-25 15:32:01 +0200

    fix: Refactor HCP downloads

--- modules/hcp/__init__.py
+++ modules/hcp/__init__.py
@@ -1,12 +1,10 @@
 """Module for fetching files from HCP."""
 
 import sys
-from concurrent.futures import Future, ProcessPoolExecutor
-from functools import partial
+from concurrent.futures import ThreadPoolExecutor, Future, as_completed
 from logging import LoggerAdapter
 from pathlib import Path
 from typing import Sequence
-
 from attrs import define, field
 from cellophane import cfg, data, modules
 from NGPIris import hcp
@@ -30,8 +28,10 @@ class HCPSample(data.Sample):
 def _fetch(
     config: cfg.Config,
     local_path: Path,
-    remote_key: str | None = None,
-) -> None:
+    remote_key: str,
+    s_idx: int,
+    f_idx: int,
+) -> tuple[int, int, Path]:
     sys.stdout = open(
         config.logdir / f"iris.{local_path.name}.out", "w", encoding="utf-8"
     )
@@ -44,40 +44,14 @@ def _fetch(
         bucket="data",  # FIXME: make this configurable
     )
 
-    if remote_key is None:
-        search_path = local_path
-        while Path(search_path).suffix:
-            search_path = search_path.with_suffix("")
+    hcpm.download_file(
+        remote_key,
+        local_path=str(local_path),
+        callback=False,
+        force=True,
+    )
 
-        result = hcpm.search_objects(search_path.name)
-        if result is not None and len(result) == 1:
-            remote_key = result[0].key
-        else:
-            raise ValueError(f"Could not find remote key for {local_path}")
-    if not local_path.exists():
-        hcpm.download_file(
-            remote_key,
-            local_path=str(local_path),
-            callback=False,
-            force=True,
-        )
-
-
-def _fetch_callback(
-    future: Future,
-    /,
-    logger: LoggerAdapter,
-    samples: data.Samples,
-    local_path: Path,
-    s_idx: int,
-    f_idx: int,
-):
-    if (exception := future.exception()) is not None:
-        logger.error(f"Failed to fetch {local_path} from HCP ({exception})")
-        samples[s_idx].files[f_idx] = None
-    else:
-        logger.debug(f"Fetched {local_path}")
-        samples[s_idx].files[f_idx] = str(local_path)
+    return s_idx, f_idx, local_path
 
 
 @modules.pre_hook(label="HCP", after=["slims_fetch"])
@@ -88,40 +62,36 @@ def hcp_fetch(
     **_,
 ) -> data.Samples:
     """Fetch files from HCP."""
-    with ProcessPoolExecutor(config.iris.parallel) as pool:
+    _futures: list[Future] = []
+    with ThreadPoolExecutor(max_workers=config.iris.parallel) as pool:
         for s_idx, sample in enumerate(samples):
-            if all(
-                file is not None and Path(file).exists() for file in sample.files or []
-            ):
+            if sample.files and all(Path(file).exists() for file in sample.files):
                 logger.debug(f"Files found for {sample.id} {sample.files}")
-
-            elif sample.backup:
+            elif sample.backup is not None:
                 logger.info(f"Fetching files for {sample.id} from HCP")
-                if "backup" not in sample:
-                    logger.warning(f"Remote key not found for {sample.id}, will search")
-                    sample.backup = [None] * len(sample.files)
-
-                for f_idx, local_key in enumerate(sample.files):
-                    remote_key: str | None = sample.backup[f_idx]
-                    _local_key = local_key or remote_key or f"{sample.id}_{f_idx}"
-                    local_path = config.iris.fastq_temp / Path(_local_key).name
-
-                    pool.submit(
+                samples[s_idx].files = []
+                for f_idx, remote_key in enumerate(sample.backup):
+                    local_path = config.iris.fastq_temp / Path(remote_key).name
+                    _future = pool.submit(
                         _fetch,
                         config=config,
                         local_path=local_path,
                         remote_key=remote_key,
-                    ).add_done_callback(
-                        partial(
-                            _fetch_callback,
-                            logger=logger,
-                            samples=samples,
-                            local_path=local_path,
-                            s_idx=s_idx,
-                            f_idx=f_idx,
-                        )
+                        s_idx=s_idx,
+                        f_idx=f_idx,
                     )
-            else:
-                logger.warning(f"Unable to fetch files for {sample.id} from HCP")
+                    _futures.append(_future)
 
-    return samples.__class__([s for s in samples if s is not None])
+    _failed: list[int] = []
+    for f in as_completed(_futures):
+        try:
+            s_idx, f_idx, local_path = f.result()
+        except Exception as e:
+            logger.error(f"Failed to fetch {local_path} from HCP ({e})")
+            samples[s_idx].files = []
+            _failed.append(s_idx)
+        else:
+            if s_idx not in _failed:
+                samples[s_idx].files[f_idx].append(local_path)
+
+    return samples
