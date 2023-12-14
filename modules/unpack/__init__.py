e19b875fc1dfe9b6e2f38a026305508b97274ad0
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-12-14 16:25:44 +0100

    fix: Await callback execution

--- modules/unpack/__init__.py
+++ modules/unpack/__init__.py
@@ -1,6 +1,7 @@
 """Module for fetching files from HCP."""
 
 import multiprocessing as mp
+from functools import partial
 from logging import LoggerAdapter
 from pathlib import Path
 
@@ -13,6 +14,37 @@ extractors: dict[str, Extractor] = {
     ".fasterq": PetageneExtractor(),
     ".spring": SpringExtractor(),
 }
+
+
+def _callback(
+    *args,
+    extractor: Extractor,
+    sample: data.Sample,
+    idx: int,
+    logger: LoggerAdapter,
+    path: Path,
+) -> None:
+    del args  # Unused
+    extracted_paths = [*extractor.extracted_paths(path)]
+    for extracted_path in extracted_paths:
+        logger.debug(f"Extracted {extracted_path.name}")
+        sample.files.insert(idx, extracted_path)
+    if path in sample.files:
+        sample.files.remove(path)
+    if not extracted_paths:
+        logger.error(f"Failed to extract {path.name}")
+
+
+def _error_callback(
+    *args,
+    sample: data.Sample,
+    logger: LoggerAdapter,
+    path: Path,
+) -> None:
+    del args  # Unused
+    logger.error(f"Failed to extract {path.name}")
+    if path in sample.files:
+        sample.files.remove(path)
 
 
 @modules.pre_hook(label="unpack", after=["hcp_fetch"])
@@ -32,34 +64,27 @@ def unpack(
         for i, p in enumerate(s.files)
         if Path(p).suffix in extractors
     ):
-
-        def _callback(*args) -> None:
-            del args  # Unused
-            nonlocal logger
-            extracted_paths = [*extractor.extracted_paths(path)]
-            for extracted_path in extracted_paths:
-                logger.debug(f"Extracted {extracted_path.name}")
-                sample.files.insert(idx, extracted_path)
-            if path in sample.files:
-                sample.files.remove(path)
-            if not extracted_paths:
-                logger.error(f"Failed to extract {path.name}")
-
-        def _error_callback(*args) -> None:
-            del args  # Unused
-            if path in sample.files:
-                sample.files.remove(path)
-
         if result := extractor.extract(
             logger=logger,
             compressed_path=path,
             config=config,
             executor=executor,
-            callback=_callback,
-            error_callback=_error_callback,
+            callback=partial(
+                _callback,
+                extractor=extractor,
+                sample=sample,
+                idx=idx,
+                logger=logger,
+                path=path,
+            ),
+            error_callback=partial(
+                _error_callback,
+                sample=sample,
+                logger=logger,
+                path=path,
+            ),
         ):
             results.append(result)
 
     executor.wait()
-    logger.warning([s.files for s in samples])
     return samples
