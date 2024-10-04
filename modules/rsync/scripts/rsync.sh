e9aaeb057831710c9642cca64ecb3379579a20a0
dodslaser <2969909+dodslaser@users.noreply.github.com>
2024-10-04 13:31:58 +0200

    fix: Include -a flag when logging rsync commands

--- modules/rsync/scripts/rsync.sh
+++ modules/rsync/scripts/rsync.sh
@@ -2,6 +2,6 @@
 set -e -o pipefail
 
 while IFS= read -r line; do
-  printf 'rsync %s\n' "$line"
-  rsync -a --no-compress $line
+  printf 'rsync -a %s\n' "$line"
+  rsync -a $line
 done < ${MANIFEST}
