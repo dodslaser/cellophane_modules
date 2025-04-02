32f4cca01a47f2f482718e83b09fc6c69f83fd1d
dodslaser <2969909+dodslaser@users.noreply.github.com>
2025-04-02 19:04:58 +0200

    feat: Create s3 module

---
+++ modules/s3/tests/test_s3.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from cellophane.src.testing import parametrize_from_yaml
+
+_ROOT = Path(__file__).parent
+
+
+class Test_integration:
+    @staticmethod
+    @parametrize_from_yaml([_ROOT / "integration.yaml"])
+    def test_integration(definition: Path, run_definition):
+        run_definition(definition)
